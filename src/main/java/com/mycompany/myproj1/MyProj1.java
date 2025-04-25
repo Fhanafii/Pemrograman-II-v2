96857c8bd61116122bdd0a4c3ccd4f4b225e17bb
Fhanafii <fhanafi544@gmail.com>
2025-04-25 20:36:48 +0700

    inital project

---
+++ src/main/java/com/mycompany/myproj1/MyProj1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.myproj1;
+
+import view.Login;
+
+/**
+ *
+ * @author fhana
+ */
+public class MyProj1 {
+
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+        login.setLocationRelativeTo(null);
+    }
+}
